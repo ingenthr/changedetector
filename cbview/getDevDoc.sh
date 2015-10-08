21e241e0a64a11363712ef551d480cde85480302
Matt Ingenthron <ingenthr@cep.net>
2015-10-08 13:00:16 -0700

    Updated design doc backup.

--- cbview/getDevDoc.sh
+++ cbview/getDevDoc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -k -u Administrator -X GET https://reporting
-.internal.couchbase.com:18092/s3logs/_design/dev_analytics -o devanalytics.json
+curl -k -u Administrator -X GET https://reporting.internal.couchbase.com:18092/s3logs/_design/dev_analytics -o dev_analytics.json
+curl -k -u Administrator -X GET https://reporting.internal.couchbase.com:18092/s3logs/_design/analytics -o analytics.json
